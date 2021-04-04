07c2d34c2d4895eadea1242939857692610bac93
Aiden Orpington <79340433+MrHotPockets@users.noreply.github.com>
2021-04-04 14:53:10 -0500

    Optimized weapon loading, added mob loading

--- Damage Calculator/Functions/insertobject.js
+++ Damage Calculator/Functions/insertobject.js
@@ -6,33 +6,80 @@ const loadWeapon = (selectedweapon) => {
     //Loads up the weapon.json
     const weaponspath = path.normalize('./damage_calculatons/../Mobs and Weapons/weapons.json')
     var file_content = fs.readFileSync(weaponspath);
+    console.log(weaponspath)
     var weaponjson = JSON.parse(file_content);
     //Searches for each weapon
     selectedweapon = selectedweapon.toLowerCase()
     let weaponUsed;
-    weaponjson.forEach(mctype => {
-      mctype.weapon.forEach(category => {
-        category.material.forEach(material => {
-          if (material.weapon.toLowerCase() === selectedweapon) {
-            const { weapon, damage, attackspeed, sharpness, strength } = material;
-            //Renames keys in object
-            weaponUsed = {
-              NAME: weapon,
-              ATK: damage,
-              ATKSPEED: attackspeed,
-              SHARP: sharpness,
-              STR: strength
+      for(const mctype of weaponjson){
+          for(const weapontype of mctype.weapon){
+            for(const material of weapontype.material){
+              if (material.weapon.toLowerCase() === selectedweapon) {
+                if (material.weapon.toLowerCase() === selectedweapon) {
+                  const { weapon, damage, attackspeed, sharpness, strength } = material;
+                  //Renames keys in object
+                  weaponUsed = {
+                    NAME: weapon,
+                    ATK: damage,
+                    ATKSPEED: attackspeed,
+                    SHARP: sharpness,
+                    STR: strength
+                  }
+                    return(weaponUsed)
+                }
+              }  
             }
-  
           }
-        })
-      })
-    })
-    return (weaponUsed)
+      }
+  }
+
+  const loadMob = (selectedmob, selecteddifficulty) => {
+    //Loads up the weapon.json
+    const mobspath = path.normalize('./damage_calculatons/../Mobs and Weapons/mobs.json')
+    var file_content = fs.readFileSync(mobspath);
+    var mobsjson = JSON.parse(file_content);
+    //Searches for each weapon
+    selectedmob = selectedmob.toLowerCase()
+    let mobUsed;
+      for(const mctype of mobsjson){
+        for(const mobs of mctype.mobs){
+          if (mobs.mob.toLowerCase() === selectedmob){
+            let { mob, type, health, armor, toughness, protection, resistance, attack} = mobs
+            console.log(mobs)
+            let isDifficultyInObject = attack.hasOwnProperty(selecteddifficulty)            
+            if (isDifficultyInObject === true){
+              for (let [key, value] of Object.entries(attack)){
+                if (key === selecteddifficulty){
+                  attack = value
+                  break;
+                }
+              }
+            }
+            else{throw new Error(`Difficulty ${selecteddifficulty} is not contained in ${mob}. Perhaps you meant easy, normal, or hard?`)}
+
+              mobUsed = {
+                NAME: mob,
+                HEALTH: health,
+                ARM: armor,
+                TOUGH: toughness,
+                PROT: protection,
+                RES: resistance,
+                ATK: attack
+              }
+              
+                return(mobUsed)
+            }
+        }
+      }
+
+
   }
 
 
 exports.loadWeapon = loadWeapon
+exports.loadMob = loadMob
 
-const weaponUsed = loadObject('Gold Sword')
+const weaponUsed = loadWeapon('Gold Sword')
 console.log(weaponUsed)
+
+console.log(loadMob('zombie', 'medium'))
